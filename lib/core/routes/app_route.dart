b059e93b4269160b5131d2dede87af5d921c9539
Abdulwahed <abdulwahedaldaghir0@gmail.com>
2026-02-06 19:24:23 +0300

    Add app route constants for navigation in app_route.dart

---
+++ lib/core/routes/app_route.dart
@@ -0,0 +1,9 @@
+class AppRoute {
+  static const String home = '/home';
+  static const String authGate = '/';
+  static const String login = '/login';
+  static const String signUp = '/signUp';
+  static const String quizPage = '/quizPage';
+  static const String summaryPage = '/summaryPage';
+  static const String assignmentPage = '/assignmentPage';
+}
