fb1c9a96c5e10a2acba3cbec224bcb0c052bef55
Abdulwahed <abdulwahedaldaghir0@gmail.com>
2026-02-06 19:24:01 +0300

    Add color constants for app theming in color.dart

---
+++ lib/core/constant/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Appcolor {
+  static const Color primaryColor =  Color(0xFF3A8DFF);
+  static const Color secondaryColor = Color(0xFFFFD66B);
+  static const Color tertiaryColor =Color(0xFF0F172A);
+
+}
