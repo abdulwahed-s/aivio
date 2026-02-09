d88e7d80d3da924842701557aad5081585a6957c
Abdulwahed <abdulwahedaldaghir0@gmail.com>
2026-02-09 18:47:41 +0400

    Add ThemeCubit for managing theme modes, including loading and updating theme preferences using SharedPreferences.

---
+++ lib/cubit/theme/theme_cubit.dart
@@ -0,0 +1,43 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeCubit extends Cubit<ThemeMode> {
+  ThemeCubit() : super(ThemeMode.system) {
+    _loadTheme();
+  }
+
+  static const String _themeKey = 'theme_mode';
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedTheme = prefs.getString(_themeKey);
+    if (savedTheme != null) {
+      if (savedTheme == 'light') {
+        emit(ThemeMode.light);
+      } else if (savedTheme == 'dark') {
+        emit(ThemeMode.dark);
+      } else {
+        emit(ThemeMode.system);
+      }
+    }
+  }
+
+  Future<void> updateTheme(ThemeMode mode) async {
+    emit(mode);
+    final prefs = await SharedPreferences.getInstance();
+    String themeString;
+    switch (mode) {
+      case ThemeMode.light:
+        themeString = 'light';
+        break;
+      case ThemeMode.dark:
+        themeString = 'dark';
+        break;
+      case ThemeMode.system:
+        themeString = 'system';
+        break;
+    }
+    await prefs.setString(_themeKey, themeString);
+  }
+}
